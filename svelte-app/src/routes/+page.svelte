5ce96437cffd000c377723cfb08aa2f8d7440965
cyko11 <liam.cyko11@gmail.com>
2023-05-17 21:28:41 +0200

    init

---
+++ svelte-app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="bg-primary w-screen h-screen fixed">
+    <div class="flex-row m-5 h-10 bg-headers rounded-full flex-grow">
+        <img class="rounded-full h-10 w-24 object-cover hover:object-none" src="cook.gif" alt="why"/>
+    </div>
+    <div class="flex-col m-5 w-10 bg-headers rounded-full flex-1 flex">
+    </div>
+    <!-- <div class="bg-headers fixed mt-20 ml-20 mr-20 rounded-e-xl">
+        
+    </div> -->
+</div>
---
+++ svelte-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors:{
+        primary:'rgb(17,17,17)',
+        secondary:'rgb(41,41,41)',
+        headers:'rgb(69,79,255)',
+      }
+    },
+  },
+  plugins: [],
+}
+
